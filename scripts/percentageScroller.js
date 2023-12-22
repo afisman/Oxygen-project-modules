fd6bab36c37267781cebf63153950844e253eae7
afisman <afisman@gmail.com>
2023-12-22 16:32:10 +0100

    fix: scroll script

--- scripts/percentageScroller.js
+++ scripts/percentageScroller.js
@@ -3,7 +3,7 @@ window.onscroll = () => {
 }
 
 const scrollFunction = () => {
-    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let windowScroll = document.documentElement.scrollTop;
     let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     let percentageScrolled = (windowScroll / windowHeight) * 100;
     document.getElementById("scroll_bar").style.width = `${percentageScrolled}%`
