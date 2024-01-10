7624895e57decbb8f886b8c8ea67d87d3ec76606
afisman <afisman@gmail.com>
2024-01-10 17:14:15 +0100

    fixed: slider added dots and functionality for autoChange

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,12 +4,19 @@ class Slider {
         this.slider = document.getElementById(slider);
         this.buttonPrevious = document.getElementById('slider_button_prev');
         this.buttonNext = document.getElementById('slider_button_next');
-        this.img = document.querySelectorAll(".slider__img");
+        this.img = document.querySelectorAll(".slider__img__container__img");
+        this.dot = document.querySelectorAll(".slider__dot__container__dot")
         this.caption = document.getElementById('image_caption');
         this.indexImg = 0;
         this.showImage(this.indexImg);
-        this.beforeImage();
-        this.nextImage();
+        this.selectDot(this.img);
+        this.beforeImage = this.beforeImage.bind(this);
+        this.nextImage = this.nextImage.bind(this);
+        this.buttonNext.addEventListener('click', this.nextImage);
+        this.buttonPrevious.addEventListener('click', this.beforeImage);
+        this.autoChange = setTimeout(() => {
+            this.nextImage();
+        }, 5000);
     }
 
     showImage(index) {
@@ -17,28 +24,44 @@ class Slider {
             if (i === index) {
                 this.caption.innerText = image.alt;
                 image.style.display = "block";
+                this.dot[i].classList.add('active');
             } else {
                 image.style.display = "none";
+                this.dot[i].classList.remove('active');
             }
         });
-    }
-    beforeImage() {
-        this.buttonPrevious.addEventListener("click", () => {
-            if (this.indexImg === 0) {
-                this.indexImg = this.img.length - 1;
-            } else {
-                this.indexImg = (this.indexImg - 1) % this.img.length;
-            }
-            this.showImage(this.indexImg);
-        });
-    }
-    nextImage() {
-        this.buttonNext.addEventListener("click", () => {
-            this.indexImg = (this.indexImg + 1) % this.img.length;
-            this.showImage(this.indexImg);
-        });
+
+        clearTimeout(this.autoChange);
+        this.autoChange = setTimeout(() => {
+            this.nextImage();
+        }, 5000);
     }
 
+    selectDot(images) {
+        window.onclick = e => {
+            clearInterval(this.autoChange);
+            if (e.target.className === 'slider__dot__container__dot') {
+                for (let i = 0; i < images.length; i++) {
+                    if (e.target.id == i) {
+                        this.indexImg = i;
+                        this.showImage(this.indexImg)
+                    }
+                }
+            }
+        }
+    }
+    beforeImage() {
+        if (this.indexImg === 0) {
+            this.indexImg = this.img.length - 1;
+        } else {
+            this.indexImg = (this.indexImg - 1) % this.img.length;
+        }
+        this.showImage(this.indexImg);
+    }
+    nextImage() {
+        this.indexImg = (this.indexImg + 1) % this.img.length;
+        this.showImage(this.indexImg);
+    }
 
 }
 
